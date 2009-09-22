6d2abc58f8fa2a9a811406e95217f6b64d7282bc
Jon Hinson <hinson.jonathan@gmail.com>
2009-09-22 08:52:44 -0500

    added mysql and some nginx config stuff. removed dna.json from repo

--- cookbooks/passenger/recipes/default.rb
+++ cookbooks/passenger/recipes/default.rb
@@ -1,3 +1,5 @@
+gem_package 'passenger'
+
 execute "install-passenger-and-nginx" do
   command "passenger-install-nginx-module --auto --auto-download --prefix=/opt/nginx"
   action :nothing
@@ -6,4 +8,24 @@ end
 execute "run-install-passenger-and-nginx" do
   command "echo installing passenger"
   notifies :run, [ resources(:execute => "install-passenger-and-nginx") ], :immediately
+end
+
+service "nginx" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+
+template "nginx.conf" do
+  path "#{node[:nginx][:dir]}/nginx.conf"
+  source "nginx.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+template "#{node[:nginx][:dir]}/sites-available/default" do
+  source "default-site.erb"
+  owner "root"
+  group "root"
+  mode 0644
 end
