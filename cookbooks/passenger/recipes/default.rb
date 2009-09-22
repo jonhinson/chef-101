5743a0598cdf0d54f1dcfab93dc6958f6e889322
Jon Hinson <hinson.jonathan@gmail.com>
2009-09-22 06:15:10 -0500

    modified passenger recipe because command wasn't running

--- cookbooks/passenger/recipes/default.rb
+++ cookbooks/passenger/recipes/default.rb
@@ -2,3 +2,8 @@ execute "install-passenger-and-nginx" do
   command "passenger-install-nginx-module --auto --auto-download --prefix=/opt/nginx"
   action :nothing
 end
+
+execute "run-install-passenger-and-nginx" do
+  command "echo installing passenger"
+  notifies :run, [ resources(:execute => "install-passenger-and-nginx") ], :immediately
+end
