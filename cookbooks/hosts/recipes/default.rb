52e8941f72617d3d09b61aee2d93cb1029621654
Jon Hinson <hinson.jonathan@gmail.com>
2009-09-24 11:31:22 -0500

    fixed postfix virtual mappings

--- cookbooks/hosts/recipes/default.rb
+++ cookbooks/hosts/recipes/default.rb
@@ -5,3 +5,13 @@ end
 template "/etc/hostname" do
   source "hostname.erb"
 end
+
+execute "hostname" do
+  command "hostname -F /etc/hostname"
+  action :nothing
+end
+
+execute "run-install-passenger-and-nginx" do
+  command "echo updating hostname"
+  notifies :run, [ resources(:execute => "hostname") ], :immediately
+end
