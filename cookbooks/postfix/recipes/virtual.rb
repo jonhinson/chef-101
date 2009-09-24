057d1c68aba276314f50aedb23841aeffb87e1fe
Jon Hinson <hinson.jonathan@gmail.com>
2009-09-24 11:34:24 -0500

    fixed postfix virtual mappings

--- cookbooks/postfix/recipes/virtual.rb
+++ cookbooks/postfix/recipes/virtual.rb
@@ -11,7 +11,6 @@ end
 template "/etc/postfix/virtual" do
   source "virtual.erb"
   
-  command "echo create virtual mappings"
   notifies :run, [ resources(:execute => "update-postfix-virtual-domains") ], :immediately
   notifies :reload, resources(:service => "postfix")
 end
